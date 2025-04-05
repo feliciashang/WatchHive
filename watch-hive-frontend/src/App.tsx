02f33ead187776e332ce5cc0971dd8b83de57fc7
feliciashang <feliciashang@gmail.com>
2025-04-05 16:17:32 -0400

    add a side panel

--- watch-hive-frontend/src/App.tsx
+++ watch-hive-frontend/src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+async function App() {
+  let [tab] = await chrome.tabs.query({ active: true});
+  chrome.sidePanel.setOptions({
+    tabId: tab.id,                // Set the current tab's ID
+    path: 'sidepanel.html',        // Path to the HTML file for the side panel
+    enabled: true                  // Enable the side panel
+  });
 }
 
 export default App
--- watch-hive-frontend/vite.config.ts
+++ watch-hive-frontend/vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/manifest.json',
-          dest: '.',
-        }
-      ],
-    }),
-  ],
+  plugins: [react()],
   build: {
-    outDir: 'build',
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: resolve(__dirname, 'index.html'),
+        sidepanel: resolve(__dirname, 'sidepanel.html'),
       },
     },
   },
-});
+})
